eb5350ac741b432ee906a14c3e097b4258d7d480
berk <badday182@gmail.com>
2025-07-30 11:45:05 +0300

    feat(tasks): configure TypeORM and add Task entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'tasks_db',
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true, // do not use in production, it can drop your database
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
